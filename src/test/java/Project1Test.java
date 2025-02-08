c540f4ccfb230c7d02e124be1db445bf44f8fc0e
aantoski <aantoski96@gmail.com>
2025-02-08 17:01:26 -0500

    fixed test

--- src/test/java/Project1Test.java
+++ src/test/java/Project1Test.java
@@ -4,11 +4,25 @@ import tasks.*;
 
 public class Project1Test {
     // Change path as needed!
-//    Sandi Path
-//    public final static String path = "/Users/antoski/WPI/CS4433/Big_Data_Management/";
+    // Sandi Path
+    // public final static String path =
+    // "/Users/antoski/WPI/CS4433/Big_Data_Management/";
 
-//    Phong Path
+    // Phong Path
     public final static String path = "C:/Study/CS4433/Big_Data_Management/";
+    public final static String output = path + "output/";
+
+    @Test
+    public void optimizedTaskA() {
+        String[] input = new String[2];
+        input[0] = path + "pages.csv";
+        input[1] = output + "optimized-A";
+        try {
+            TaskA.optimized(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void basicTaskC() {
@@ -90,7 +104,7 @@ public class Project1Test {
         input[2] = output + "optimized-G";
         try {
             System.out.println("Input Path: " + input[0]);
-            taskG.debug(input);
+            taskG.optimized(input);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -115,9 +129,9 @@ public class Project1Test {
     public void testTaskD() {
         TaskD taskD = new TaskD();
         String[] input = new String[3];
-        input[0] = path + "friends.csv";  // Friendship file
-        input[1] = path + "output/D";     // Output directory
-        input[2] = path + "pages.csv";    // Page owners file
+        input[0] = path + "friends.csv"; // Friendship file
+        input[1] = path + "output/D"; // Output directory
+        input[2] = path + "pages.csv"; // Page owners file
         try {
             System.out.println("Input Path: " + input[0]);
             taskD.debug(input);
@@ -130,10 +144,10 @@ public class Project1Test {
     public void testTaskF() {
         TaskF taskF = new TaskF();
         String[] input = new String[4];
-        input[0] = path + "friends.csv";      // Friendship file
-        input[1] = path + "access_logs.csv";  // Access logs file
-        input[2] = path + "output/F";         // Output directory
-        input[3] = path + "pages.csv";        // Person details file
+        input[0] = path + "friends.csv"; // Friendship file
+        input[1] = path + "access_logs.csv"; // Access logs file
+        input[2] = path + "output/F"; // Output directory
+        input[3] = path + "pages.csv"; // Person details file
         try {
             System.out.println("Input Path: " + input[0]);
             taskF.debug(input);
@@ -146,10 +160,10 @@ public class Project1Test {
     public void testTaskH() {
         TaskH taskH = new TaskH();
         String[] input = new String[4];
-        input[0] = path + "friends.csv";           // Friend relationships file
-        input[1] = path + "pages.csv";             // People details file
-        input[2] = path + "output/intermediate";   // Intermediate output directory for Job 1
-        input[3] = path + "output/popular";        // Final output directory for Job 2
+        input[0] = path + "friends.csv"; // Friend relationships file
+        input[1] = path + "pages.csv"; // People details file
+        input[2] = path + "output/intermediate"; // Intermediate output directory for Job 1
+        input[3] = path + "output/popular"; // Final output directory for Job 2
         try {
             System.out.println("Testing TaskH with input: " + input[0]);
             taskH.debug(input);
